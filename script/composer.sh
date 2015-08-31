c1dedadc1d2fd7d9813199195d2992c0765a742c
Nicholas J <njohns@grovo.com>
2015-08-31 19:36:38 -0400

    Added servox

---
+++ script/composer.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install -y php5-cli php5-common php5-mcrypt php5-dev php5-xdebug php5-intl php5-curl php5-mysqlnd php5-sqlite php5-pgsql vim git curl htop
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
---
+++ script/docker-compose.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install -y python-pip
+sudo pip install -U docker-compose==1.4.0
+
